adf506e4a6117c1c7f6c91ffb9aada7a36c68f17
takna <tak_na@icloud.com>
2023-02-02 14:10:39 +0900

    Radix Primitives の Popover の実装(スタイルあり)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
+import { useState } from 'react'
 import * as Popover from '@radix-ui/react-popover'
 import './index.css'
 
 const App = () => {
+  const [open, setOpen] = useState(false)
+
   return (
-    <>
-      <PopoverDemo />
-    </>
+    <Popover.Root open={open} onOpenChange={setOpen}>
+      <Popover.Trigger
+        className={`${open ? 'bg-blue-200' : 'bg-gray-200'} rounded px-2 py-1 shadow`}
+      >
+        More info
+      </Popover.Trigger>
+      <Popover.Portal>
+        <Popover.Content className='max-w-sm rounded-xl bg-red-200 px-2 py-4'>
+          Some more info… Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut 500 labore et dolore magna aliqua. Ut enim ad minim veniam
+          <Popover.Arrow className='fill-red-200' />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   )
 }
-
-const PopoverDemo = () => (
-  <Popover.Root>
-    <Popover.Trigger>More info</Popover.Trigger>
-    <Popover.Portal>
-      <Popover.Content>
-        Some more info…
-        <Popover.Arrow />
-      </Popover.Content>
-    </Popover.Portal>
-  </Popover.Root>
-)
 
 export default App
