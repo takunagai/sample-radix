eff65a79fc4101baf01cec0d5c00724bc9f49364
takna <tak_na@icloud.com>
2023-02-02 10:03:15 +0900

    Radix Primitives の Popover の実装(スタイルなし)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import * as Popover from '@radix-ui/react-popover'
 import './index.css'
 
-function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+const App = () => {
+  return (
+    <>
+      <PopoverDemo />
+    </>
+  )
 }
+
+const PopoverDemo = () => (
+  <Popover.Root>
+    <Popover.Trigger>More info</Popover.Trigger>
+    <Popover.Portal>
+      <Popover.Content>
+        Some more info…
+        <Popover.Arrow />
+      </Popover.Content>
+    </Popover.Portal>
+  </Popover.Root>
+)
 
 export default App
