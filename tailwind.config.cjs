b4c3dda8b973b6dc29a1b65990b226143af6b4d9
takna <tak_na@icloud.com>
2023-02-04 20:27:26 +0900

    TailwindCSS にテーマカラー primary を追加

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,22 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#FFF2EB",
+          100: "#FFE6D6",
+          200: "#FFD0B3",
+          300: "#FFB78A",
+          400: "#FFA166",
+          500: "#FF883F",
+          600: "#FF6200",
+          700: "#BD4800",
+          800: "#803100",
+          900: "#3D1700"
+        },
+      }
+    },
   },
   plugins: [],
 }
